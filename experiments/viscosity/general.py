822ce0daecf3e20da0dee6a5c8bed842fd72ffc3
jona ackerschott <jona.a99@gmail.com>
2019-06-28 14:05:09 +0200

    experiments: viscosity: general.py: Minor changes.

--- experiments/viscosity/general.py
+++ experiments/viscosity/general.py
@@ -3,13 +3,13 @@ import datproc.print as dpr
 import stokes as st
 import hagen_poiseuille as hp
 
-print(dpr.val('η_st', st.eta, st.d_eta, unit='Pa s'))
-print(dpr.val('η_hp', hp.eta, hp.d_eta, unit='Pa s'))
+print(dpr.val(st.eta, st.d_eta, name='η_st', unit='Pa s'))
+print(dpr.val(hp.eta, hp.d_eta, name='η_hp', unit='Pa s'))
 print(dpr.dev(st.eta, st.d_eta, hp.eta, hp.d_eta, name='η_st, η_hp'))
 
 Re_crit = 3
 d_Re_crit = 1
 Re_crit_theo = 1
-print(dpr.val('Re_crit', Re_crit, d_Re_crit))
-print(dpr.val('Re_crit_theo', Re_crit_theo))
+print(dpr.val(Re_crit, d_Re_crit, name='Re_crit'))
+print(dpr.val(Re_crit_theo, name='Re_crit_theo'))
 print(dpr.dev(Re_crit, d_Re_crit, Re_crit_theo, name='Re_crit, Re_crit_theo'))
